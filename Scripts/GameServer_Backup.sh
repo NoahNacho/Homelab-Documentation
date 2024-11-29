0f7795537a2ba909dc7bfd5039632ec0e4cd05fb
Noah <63881698+NoahNacho@users.noreply.github.com>
2024-11-29 01:33:40 -0500

    Update GameServer_Backup.sh
    Change from pigz to zstd
    zstd is built in, less likely for errors, just as fast and good

--- Scripts/GameServer_Backup.sh
+++ Scripts/GameServer_Backup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-#basic backup script will make nice later
-
-#Prefix allows you to put all game servers in one dir and back them all up at once
-Prefix=""
+Prefix="/home/noah/Games"
 Names=("Project-Zomboid" "Minecraft-Modded" "Minecraft-Vanilla" "Satisfactory" "Terraria")
 Dirs=("$Prefix/PZ/data/Saves/Multiplayer/" "$Prefix" "$Prefix" "$Prefix/Satisfactory/config/" "$Prefix/Terraria/")
 Dirs2=("./servertest/" "./Minecraft-Modded/" "./Minecraft-Vanilla/" "./saved/" "./world/")
@@ -11,13 +8,14 @@ Date=$(date +'%H:%M:%S')
 Time=$(date +'%H:%M:%S')
 echo "$Time - Starting script." >> Logs/$Date.log
 for i in ${!Dirs[@]}; do
-  tar -c --use-compress-program="pigz -p 8" -f ${Names[i]}/$Date.tar.gz -C ${Dirs[i]} ${Dirs2[i]} >> Logs/$Date.log
+  tar --zstd -cf ${Names[i]}/$Date.tar.zst -C ${Dirs[i]} ${Dirs2[i]} >> Logs/$Date.log
   Time=$(date +'%H:%M:%S')
-  echo "$Time - Compressed ${Names[i]} to ./Backups/${Names[i]} as $Date.tar.gz" >> Logs/$Date.log
+  echo "$Time - Compressed ${Names[i]} to ./Backups/${Names[i]} as $Date.tar.zst" >> Logs/$Date.log
 done
 
 #remove tar files that are 30 days old
 Time=$(date +'%H:%M:%S')
 echo "$Time - Old Backups Getting Removed:" >> Logs/$Date.log
-find /home/noah/Backups/ -mtime +0 -type f -name \*.tar.gz >> Logs/$Date.log
-find /home/noah/Backups/ -mtime +0 -type f -name \*.tar.gz -delete
+find /home/noah/Backups/ -mtime +0 -type f -name \*.tar.zst >> Logs/$Date.log
+find /home/noah/Backups/ -mtime +0 -type f -name \*.tar.zst -delete
+
